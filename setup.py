9aead733645a960d41c2b2b1d2ddb980481f4848
Kirill Pekarov <kpekarov@gmail.com>
2014-01-09 21:32:11 +0200

    fixed name and repository path

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from distutils.core import setup
 
 setup(
     version='0.1',
-    name='django_dynamicsites',
+    name='dynamicsites',
     description="Host multiple sites from a single django project",
     url='https://github.com/kpekarov/django-dynamicsites',
     platforms=['any'],
-    package_dir={'django_dynamicsites': "dynamicsites"},
-    packages=['django_dynamicsites'],
+    packages=['dynamicsites'],
 )
